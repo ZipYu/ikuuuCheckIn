6bb774f38ceb1bd749c543f5b955042dc103f502
Dsiny Yu <davedqaq@outlook.com>
2023-06-30 17:19:23 +0800

    Update signin.js
    格式化代码

--- signin.js
+++ signin.js
@@ -5,33 +5,30 @@ let expire;
 let uidStr;
 let emailStr='ur email';
 let password='ur password';
-const loginURL = 'https://ikuuu.eu/auth/login?email='+emailStr+'&passwd='+password;
-const checkinURL= 'https://ikuuu.eu/user/checkin';
+let domain = 'https://ikuuu.eu';
+const loginURL = domain + '/auth/login?email=' + emailStr + '&passwd=' + password;
+const checkinURL = domain + 'https://ikuuu.eu/user/checkin';
 axios.post(loginURL)
   .then(function (response) {
-    if(response.data.ret==0){
-      console.log('登录错误：'+response.data.msg);
-      process.exit(1);
-    }
-    console.log(response.data.msg)
     let array = response.headers['set-cookie'];
+    console.log(array)
     let keyArray = array[2].split("; ");
-    key=keyArray[0];
+    key = keyArray[0];
     let expireArray = array[4].split("; ");
     expire = expireArray[0];
     let uidArray = array[0].split("; ");
     uidStr = uidArray[0];
-    let cookieStr = key+'; '+expire+';'+uidStr+'; email='+emailStr+';';
-  console.log("脚本："+cookieStr);
+    let cookieStr = key + '; ' + expire + ';' + uidStr + '; email=' + emailStr + ';';
+    console.log("Cookie:" + cookieStr);
 
-axios.defaults.headers.common['Cookie'] = cookieStr;
-axios.post(checkinURL)
-.then(function(res){
-  console.log("info"+res.data.msg);
-})
-.catch(function(error){
-    console.log("错误信息:"+ error);
-})
+    axios.defaults.headers.common['Cookie'] = cookieStr;
+    axios.post(checkinURL)
+      .then(function (res) {
+        console.log("info" + res.data.msg);
+      })
+      .catch(function (error) {
+        console.log("错误信息:" + error);
+      })
 
 
 
