27f1f95e4cd4c44535ccfafbf0d788890aadbb56
Dsiny Yu <davedqaq@outlook.com>
2023-06-30 14:45:12 +0800

    Update signin.js

--- signin.js
+++ signin.js
@@ -3,22 +3,26 @@ const axios = require('axios')
 let key;
 let expire;
 let uidStr;
-let emailStr='ur email';
-let password='ur password';
+let emailStr='davedqaq@outlook.co';
+let password='RZ9.JnK7LWiszZv';
 const loginURL = 'https://ikuuu.eu/auth/login?email='+emailStr+'&passwd='+password;
 const checkinURL= 'https://ikuuu.eu/user/checkin';
 axios.post(loginURL)
   .then(function (response) {
+    if(response.data.ret==0){
+      console.log('登录错误：'+response.data.msg);
+      process.exit(1);
+    }
+    console.log(response.data.msg)
     let array = response.headers['set-cookie'];
-    console.log(array)
     let keyArray = array[2].split("; ");
     key=keyArray[0];
     let expireArray = array[4].split("; ");
     expire = expireArray[0];
     let uidArray = array[0].split("; ");
     uidStr = uidArray[0];
-      let cookieStr = key+'; '+expire+';'+uidStr+'; email='+emailStr+';';
-  console.log("Cookie:"+cookieStr);
+    let cookieStr = key+'; '+expire+';'+uidStr+'; email='+emailStr+';';
+  console.log("脚本："+cookieStr);
 
 axios.defaults.headers.common['Cookie'] = cookieStr;
 axios.post(checkinURL)
