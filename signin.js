9cfd9a2164f49ad5399537487b7e60b30642c052
Dsiny Yu <davedqaq@outlook.com>
2023-06-30 17:33:30 +0800

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,28 +1,20 @@
-const axios = require('axios')
-
-let key;
-let expire;
-let uidStr;
-let emailStr='ur email';
-let password='ur password';
-let domain = 'https://ikuuu.eu';
+const emailStr = 'ur email';
+const password = 'ur password';
+const domain = 'https://ikuuu.eu';
 const loginURL = domain + '/auth/login?email=' + emailStr + '&passwd=' + password;
 const checkinURL = domain + '/user/checkin';
 axios.post(loginURL)
   .then(function (response) {
-if(response.data.ret==0){
-      console.log('登录错误：'+response.data.msg);
+    if (response.data.ret == 0) {
+      console.log('登录错误：' + response.data.msg);
       process.exit(1);
     }
     console.log(response.data.msg)
     let array = response.headers['set-cookie'];
     let keyArray = array[2].split("; ");
-    key = keyArray[0];
     let expireArray = array[4].split("; ");
-    expire = expireArray[0];
     let uidArray = array[0].split("; ");
-    uidStr = uidArray[0];
-    let cookieStr = key + '; ' + expire + ';' + uidStr + '; email=' + emailStr + ';';
+    let cookieStr = keyArray[0] + '; ' + expireArray[0] + ';' + uidArray[0] + '; email=' + emailStr + ';';
 
     axios.defaults.headers.common['Cookie'] = cookieStr;
     axios.post(checkinURL)
